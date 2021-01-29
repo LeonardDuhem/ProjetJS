e7896708fadc9a7587db6b1a33e3f66998f58f8f
allan <allan76440@gmail.com>
2021-01-29 10:26:19 +0100

    Commentaire + mise au propre du code

--- js/app.js
+++ js/app.js
@@ -11,12 +11,14 @@ notif.setAttribute('id', 'notification_container');
 let toutSupp = document.querySelector('#empty-cart');
 toutSupp.addEventListener('click', supprimerToutArticle)
 
+//Recherche du click sur le bouton
 cart.forEach(function (elem) {
     elem.addEventListener("click", clicBtn)
 })
 
 //Vérif du clique
 function clicBtn(e) {
+
     //Valeur en html
     const course = e.target.parentElement.parentElement;
     getInfoElem(course)
@@ -24,18 +26,23 @@ function clicBtn(e) {
 
 //Récupération valeur
 function getInfoElem(course) {
+
     //Nom du Cours
     let name = course.querySelector('h4').textContent
     console.log(name)
+
     //Prix
     let price = course.querySelector('.discount').textContent
     console.log(price)
+
     //Image
     let img = course.querySelector('img').src
     console.log(img)
+
     //Place dans la liste de cours
     let place = course.querySelector('a').getAttribute('data-id')
     console.log(place)
+
     //Nombre en Stock
     let stock = course.querySelector('.stock').textContent
     console.log(stock)
@@ -46,9 +53,9 @@ function getInfoElem(course) {
 
 //Création "thead"
 function addcart(Img, Article, Prix, Qt) {
-
+    //Tableau
     let newTr = document.createElement('tr');
-
+    //td = ligne du tableau
     let newImg = document.createElement('td');
     newImg.innerHTML += `<img src=${Img}>`;
     newTr.appendChild(newImg);
@@ -66,6 +73,8 @@ function addcart(Img, Article, Prix, Qt) {
     newQt.innerHTML = Qt;
 
     let newSupp = document.createElement('td');
+    
+    //Création de l'image du bouton supprimer
     let img = document.createElement('img');
     img.addEventListener('click', supprimerArticle);
     img.setAttribute('src', 'img/close.png');
@@ -73,8 +82,10 @@ function addcart(Img, Article, Prix, Qt) {
     newSupp.appendChild(img);
     newTr.appendChild(newSupp);
 
+    //Ajout du tableau dans le tbody 
     tbody.appendChild(newTr);
 
+    //Local Storage
     let teste = {
         'Article': Article,
         'Qt': Qt,
@@ -117,13 +128,13 @@ function notifications(Article, isAjout) {
         notif.removeChild(infoNotif);
     }, 2000);
 }
-
+//Suppression d'un élement du panier
 function supprimerArticle(elem) {
     const name = elem.target.parentElement.parentElement.querySelectorAll('td')[1].textContent;
     elem.target.parentElement.parentElement.remove();
     notifications(name, false);
 }
-
+//Suppression de tout les élements du panier
 function supprimerToutArticle() {
     tbody.remove()
     document.location.reload();
